ad6a0bf7b96a0d2cf7ec4c9745e1741dc746475b
CheQuocDat <datdz0985@gmail.com>
2025-10-24 20:28:02 +0700

    Hoàn thiện giao diện quản lý nhân viên Flutter

---
+++ lib/core/api/api_client.dart
@@ -0,0 +1,6 @@
+class ApiClient {
+  // Thay đổi baseUrl theo địa chỉ server của bạn
+  //static const String baseUrl = 'http://localhost:3000/api';
+  //static const String baseUrl = 'http://10.0.2.2:3000/api/nhan-vien';
+  static const String baseUrl = "http://10.0.2.2:3000";
+}
